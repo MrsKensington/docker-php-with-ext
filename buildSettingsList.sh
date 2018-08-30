f7d806e955b6512cf1339e17b88af4d10f1ef7d4
Mike Ditum <mike.ditum@tripleplay.tv>
2018-08-30 14:27:36 +0100

    Update scripts that generate the build settings and javascript to set it

--- buildSettingsList.sh
+++ buildSettingsList.sh
@@ -1,17 +1,27 @@
 #create all of the individual ones
-for i in `find -name Dockerfile -exec dirname {} \;`; do 
-    NAME=`echo $i | sed 's#\./##g' | sed 's#/#-#g'`; 
-    echo "master $i/ $NAME"; 
+for i in `find -name Dockerfile -exec dirname {} \; | sed 's#\./##g'`; do 
+    NAME=`echo $i | sed 's#/#-#g'`; 
+    echo "master /$i/ $NAME"; 
+done
+
+#create all the minor release variants
+for i in `ls -d [0-9].[0-9]`; do
+    OS=$(ls -d $i/* | tail -n 1)
+    for k in `find $OS -mindepth 1 -type d`; do
+        NAME=`basename $k`
+        echo "master /$k/ $i-$NAME"; 
+    done
+    echo "master /$k/ $i"; 
 done
 
 #create all the major release variants
 for i in `ls -d [0-9].[0-9] | sed 's/\./ /g' | awk '{print $1}' | sort -u`; do
     for j in `ls -d $i.[0-9]/* | tail -n 1`; do
-        for k in `find ./$j -mindepth 1 -type d`; do
+        for k in `find $j -mindepth 1 -type d`; do
             NAME=`basename $k`
-            echo "master $k/ $i-$NAME"; 
+            echo "master /$k/ $i-$NAME"; 
         done
-        echo "master $k/ $i"; 
+        echo "master /$k/ $i"; 
     done
 done
 
@@ -19,8 +29,8 @@ done
 LATEST=`ls -d [0-9].[0-9]/* | sort | tail -n 1`
 for i in `find $LATEST -mindepth 1 -type d`; do
     NAME=`basename $i`
-    echo "master ./$i/ latest-$NAME"; 
+    echo "master /$i/ latest-$NAME"; 
 done
 
 #"latest" is same as latest-cli version
-echo "master ./$LATEST/cli/ latest"
+echo "master /$LATEST/cli/ latest"
--- generateBuildSettingsJavascript.sh
+++ generateBuildSettingsJavascript.sh
@@ -1,16 +1,24 @@
 cat << EOF
-function f() {
-    var values = "$(./buildSettingsList.sh | xargs echo)".split(" ");
-    var inputs = document.getElementsByClassName("SimpleInput__default___gdtxe");
-    if(inputs.length - 1 != values.length) {
-        var num = (values.length - inputs.length + 1) / 3;
-        alert("Need another " + num + " config lines");
-        return;
-    }
+# 1. Search client.whatever.js (when pretty printed) for '"/autobuild/tags/" +'
+# 2. Put a break point at the put request line
+# 3. Click Save Changes
+# 4. Go up the stack 4 times (function currently called L)
+# 5. Put a break point on the first line of that function
+# 6. Remove the other break point
+# 7. Resume script execution
+# 8. Click Save Changes Again
+# 9. Paste the code below in
+var values = "$(./buildSettingsList.sh | xargs echo)".split(" ");
 
-    for(var i=0; i<values.length; i++) {
-        inputs.item(i).value = values[i];
-    }
+var diff = values.length - (a.length * 3);
+if(diff != 0) {
+    throw new Error("Need " + (diff/3) + " more tags");
 }
-f();
+
+var index = 0;
+for(var i=0; i<a.length; i++) {
+    a[i].source_name = values[index++];
+    a[i].dockerfile_location = values[index++];
+    a[i].name = values[index++];
+}
 EOF
